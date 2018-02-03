cf2a29c787a5873759906187e92a9c44d8147d57
alexMAF93 <31741850+alexMAF93@users.noreply.github.com>
2018-02-03 23:59:53 +0200

    Update hkcard.sh

--- hkcard.sh
+++ hkcard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cale=/home/g05cpirv/loguri/health_check_Card
+cale=~/health_check_Card
 cat $cale | egrep -a "Trap raised"\|Connected\|EEST\|EET\|$B:P\|Card\|Trap\|"XPL Errors"\|"chip Errors Detect
 ed"\|"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" | egrep -va "Specific"\|logout\|"Press any key" | le
 ss
