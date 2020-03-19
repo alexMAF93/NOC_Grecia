55ff32ebcdec20289970b91031091a3cc101c047
alexMAF93 <31741850+alexMAF93@users.noreply.github.com>
2020-03-19 10:08:17 +0200

    asdas

--- hkcardrun.sh
+++ hkcardrun.sh
@@ -1,30 +1,30 @@
- #!/bin/bash
+#!/bin/bash
+
+
 filename=~/lista
 cpnnode=(`cat "$filename"`)
 cale=~/health_check_Card
 date > $cale
 echo >> $cale
-echo
-echo
-echo
+printf '\n\n\n'
 
-for i in {0..50}
 
+for i in `cat $filename | wc -l`
 do
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >>$cale
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >>$cale
 
-(echo open "${cpnnode[$i]}"
-sleep 1
-echo aaaa
-sleep 1
-echo xxxx
-sleep 1
-echo show card detail
-sleep 1
-while true
-do
-echo l
-sleep 1
-done
-) | telnet >> $cale 2>/dev/null
+    (echo open "${cpnnode[$i]}"
+    sleep 1 
+    echo aaaa
+    sleep 1
+    echo xxxx
+    sleep 1
+    echo show card detail
+	   sleep 1
+	   while true
+	   do
+		      echo l
+		      sleep 1
+	   done
+	   ) | telnet >> $cale 2>/dev/null
 done
